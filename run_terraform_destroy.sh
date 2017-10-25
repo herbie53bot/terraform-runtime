5f9a335f37957083642da62826998f19042482e4
Thijs Schnitger <thijs.schnitger@container-solutions.com>
2017-10-25 12:37:09 +0200

    also add profile when using the other commands

--- run_terraform_destroy.sh
+++ run_terraform_destroy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "[$AWS_PROFILE]" > /home/herbie_bot/.aws/credentials
 sudo find /iac -type d -exec chmod 777 {} \;
 sudo chmod +x /iac/bin/run_terraform
 /iac/bin/run_terraform destroy -force
